f2b6cd1b24a28e5fce0ac7f6971c6b4268e9172b
Bartlomiej Danielak <bartlomiejdanielak@gmail.com>
2024-08-06 16:40:27 +1000

    added markTotal

--- assessment2/Main.java
+++ assessment2/Main.java
@@ -79,12 +79,13 @@ class StudentMarks {
                     double mark1 = parseDoubleWithDefault(data[3].trim(), 0.0);
                     double mark2 = parseDoubleWithDefault(data[4].trim(), 0.0);
                     double mark3 = parseDoubleWithDefault(data[5].trim(), 0.0);
-                    
+                    double markTotal = mark1 + mark2 + mark3;
                     System.out.println("Student Name: " + firstName + " " + lastName);
                     System.out.println("Student ID: " + studentID);
                     System.out.println("A1: " + mark1);
                     System.out.println("A2: " + mark2);
                     System.out.println("A3: " + mark3);
+                    System.out.println("Total Mark: " + markTotal);
                     System.out.println();
                     
                     Student student = new Student(firstName, lastName, studentID, mark1, mark2, mark3);
